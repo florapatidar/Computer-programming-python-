94428bc6861ab37d9944c74beda77d6ddf240e83
florapatidar <fairypatidar@gmail.com>
2026-01-08 15:28:47 +0530

    Add files via upload

---
+++ 25bcp029_calculator.py
@@ -0,0 +1,26 @@
+#calculator
+def add(a,b):
+    c=a+b
+    print(c)
+
+def subtract(a,b):
+    c=a-b
+    print(c)
+
+def multiply(a,b):
+    c=a*b
+    print(c)
+
+def divide(a,b):
+    c=a/b
+    print(c)
+
+#main
+a=int(input("enter number"))
+b=int(input("enter another number"))
+add(a,b)
+subtract(a,b)
+multiply(a,b)
+divide(a,b)
+
+    
