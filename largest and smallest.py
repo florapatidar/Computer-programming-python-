80cb21a7f7d534ed2852d081ec9fd04a08c464e2
florapatidar <fairypatidar@gmail.com>
2026-01-22 14:23:17 +0530

    Add files via upload

---
+++ largest and smallest.py
@@ -0,0 +1,9 @@
+#largest and smallest value out of 2
+a=int(input("enter a number:"))
+b=int(input("enter another number:"))
+if a >b:
+    print(a,"is largest number")
+    print(b,"is smallest number")
+else:
+    print(b,"is largest number")
+    print(a,"is smallest number")
