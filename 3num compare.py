3e20c7c2d403b309cc8aaefd3ddb4ba61a46c905
florapatidar <fairypatidar@gmail.com>
2026-01-22 14:36:33 +0530

    Add files via upload

---
+++ 3num compare.py
@@ -0,0 +1,8 @@
+#larget value and smallest out of 3
+a=int(input("enter 1st number:"))
+b=int(input("enter 2nd number:"))
+c=int(input("enter 3rd number:"))
+largest=max(a,b,c)
+smallest=min(a,b,c)
+print("largest number is",largest)
+print("smallest number is", smallest)
